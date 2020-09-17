2b161d06be858c89a7de4400043241bea05866e0
Sherub Thakur <sherub.thakur@gmail.com>
2020-09-18 00:21:45 +0530

    Improve font rendering slightly

--- home.nix
+++ home.nix
@@ -394,6 +394,14 @@ in
       "*.color15" = colorscheme.white;
 
       "XTerm*font" = "xft:Hack Nerd Font Mono:pixelsize=12";
+
+      "Xft.dpi" = 96;
+      "Xft.antialias" = true;
+      "Xft.hinting" = true;
+      "Xft.rgba" = "rgb";
+      "Xft.autohint" = false;
+      "Xft.hintstyle" = "hintslight";
+      "Xft.lcdfilter" = "lcddefault";
     };
   };
 }
