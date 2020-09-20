a30e146effc86809aeb81f3c4889fc8e7d19412d
Sherub Thakur <sherub.thakur@gmail.com>
2020-09-20 19:33:02 +0530

    Remove NUR block from config

--- config.nix
+++ config.nix
@@ -1,8 +1,3 @@
 {
   allowUnfree = true;
-  packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
 }
