ac2a7850eb4b60b63176df780fac75031f1ef81e
Sherub Thakur <sherub.thakur@gmail.com>
2020-10-02 00:26:02 +0530

    Let block to top level functions in xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -20,85 +20,16 @@ import Graphics.X11.ExtraTypes.XF86
 
 main :: IO ()
 main = do
-    xmonad $ docks $ ewmh myConfig
+    xmonad . docks . ewmh $ myConfig
 
 
 myConfig =
-    let
-        myWorkspaces :: [String]
-        myWorkspaces = ["a", "s", "d", "f"]
-
-        modMask :: KeyMask
-        modMask = mod1Mask
-
-        tall :: Tall a
-        tall = Tall 1 (3/100) (1/2)
-
-        keysToRemove :: [(KeyMask, KeySym)]
-        keysToRemove = defaultWorkSpaceSwitchBinding ++ defaultWorkSpaceWindowMoveBinding
-            where
-                defaultWorkSpaceWindowMoveBinding =
-                    [(modMask .|. shiftMask, n) | n <- [xK_1 .. xK_9]] 
-                defaultWorkSpaceSwitchBinding =
-                    [(modMask, n) | n <- [xK_1 .. xK_9]] 
-
-        keysToAdd :: [((KeyMask, KeySym), X ())]
-        keysToAdd = launchers ++ multimediaKeys ++ workspaceSetup ++ layoutRelated
-            where
-                launchers =
-                    [ ( (modMask, xK_space)
-                        , spawn "rofi -show drun"
-                      )
-                    , ( (modMask .|. shiftMask, xK_l)
-                        , spawn "i3lock-fancy -p -t \"\""
-                      )
-                    , ( (modMask .|. shiftMask, xK_Return)
-                        , spawn "xterm -e \"cd $(xcwd) && ~/.nix-profile/bin/zsh\""
-                      )
-                    , ( (modMask .|. shiftMask, xK_4)
-                        , spawn "sleep 0.2 && scrot -s ~/Pictures/screenshots/scrot_%Y-%m-%d-%H%M%S.png"
-                      )
-                    ]
-                workspaceSetup = 
-                    [ ((m .|. modMask, k), windows $ f i)
-                    | (i, k) <- zip (myWorkspaces) [xK_a, xK_s, xK_d, xK_f]
-                    , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
-                multimediaKeys =
-                    [ ((0, xF86XK_AudioLowerVolume), spawn "amixer sset Master 5%-")
-                    , ((0, xF86XK_AudioRaiseVolume), spawn "amixer sset Master 5%+")
-                    , ((0, xF86XK_AudioMute       ), spawn "amixer sset Master toggle")
-                    ]
-                layoutRelated =
-                    [ ((modMask, xK_n), sendMessage NextLayout)
-                    ]
-
-        spacingLayoutSetup :: l a -> ModifiedLayout Spacing l a
-        spacingLayoutSetup = spacingRaw True screenBorder True windowBorder True
-            where
-                screenBorder = Border 5 5 5 5
-                windowBorder = Border 5 5 5 5
-
-        gapLayoutSetup :: l a -> ModifiedLayout Gaps l a
-        gapLayoutSetup = gaps [ (U, edgeGap), (R, edgeGap), (D, edgeGap), (L, edgeGap) ]
-            where edgeGap = 20
-
-
-        layoutModifiers = lessBorders OnlyScreenFloat . avoidStruts . spacingLayoutSetup . gapLayoutSetup
-
-        layouts :: Choose Tall Full a
-        layouts = tall ||| Full
-
-        myManageHook :: ManageHook
-        myManageHook = composeAll
-            [ manageDocks
-            ]
-     in
      def
          { logHook = ewmhDesktopsLogHook
          , terminal = "xterm"
          , startupHook = myStartupHook
          , manageHook =  myManageHook
-         , layoutHook = layoutModifiers layouts 
+         , layoutHook = myLayoutModifiers myLayouts 
          , workspaces = myWorkspaces
          , handleEventHook = fullscreenEventHook <+> handleEventHook def
 
@@ -109,6 +40,82 @@ myConfig =
          }
          `removeKeys` keysToRemove
          `additionalKeys` keysToAdd
+
+
+myWorkspaces :: [String]
+myWorkspaces = ["a", "s", "d", "f"]
+
+
+myModMask :: KeyMask
+myModMask = mod1Mask
+
+
+keysToRemove :: [(KeyMask, KeySym)]
+keysToRemove = defaultWorkSpaceSwitchBinding ++ defaultWorkSpaceWindowMoveBinding
+    where
+        defaultWorkSpaceWindowMoveBinding =
+            [(myModMask .|. shiftMask, n) | n <- [xK_1 .. xK_9]] 
+        defaultWorkSpaceSwitchBinding =
+            [(myModMask, n) | n <- [xK_1 .. xK_9]] 
+
+
+keysToAdd :: [((KeyMask, KeySym), X ())]
+keysToAdd = launchers ++ multimediaKeys ++ workspaceSetup ++ layoutRelated
+    where
+        launchers =
+            [ ( (myModMask, xK_space)
+                , spawn "rofi -show drun"
+              )
+            , ( (myModMask .|. shiftMask, xK_l)
+                , spawn "i3lock-fancy -p -t \"\""
+              )
+            , ( (myModMask .|. shiftMask, xK_Return)
+                , spawn "xterm -e \"cd $(xcwd) && ~/.nix-profile/bin/zsh\""
+              )
+            , ( (myModMask .|. shiftMask, xK_4)
+                , spawn "sleep 0.2 && scrot -s ~/Pictures/screenshots/scrot_%Y-%m-%d-%H%M%S.png"
+              )
+            ]
+        workspaceSetup = 
+            [ ((m .|. myModMask, k), windows $ f i)
+            | (i, k) <- zip (myWorkspaces) [xK_a, xK_s, xK_d, xK_f]
+            , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
+        multimediaKeys =
+            [ ((0, xF86XK_AudioLowerVolume), spawn "amixer sset Master 5%-")
+            , ((0, xF86XK_AudioRaiseVolume), spawn "amixer sset Master 5%+")
+            , ((0, xF86XK_AudioMute       ), spawn "amixer sset Master toggle")
+            ]
+        layoutRelated =
+            [ ((myModMask, xK_n), sendMessage NextLayout)
+            ]
+
+
+myLayoutModifiers =
+    lessBorders OnlyScreenFloat . avoidStruts . spacingLayoutSetup . gapLayoutSetup
+        where
+            spacingLayoutSetup :: l a -> ModifiedLayout Spacing l a
+            spacingLayoutSetup = spacingRaw True screenBorder True windowBorder True
+
+            gapLayoutSetup :: l a -> ModifiedLayout Gaps l a
+            gapLayoutSetup =
+                gaps [ (U, edgeGap), (R, edgeGap), (D, edgeGap), (L, edgeGap) ]
+
+            screenBorder = Border 5 5 5 5
+            windowBorder = Border 5 5 5 5
+            edgeGap = 20
+
+
+myLayouts :: Choose Tall Full a
+myLayouts = tall ||| Full
+    where
+        tall :: Tall a
+        tall = Tall 1 (3/100) (1/2)
+
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ manageDocks
+    ]
 
 
 myStartupHook :: X ()
