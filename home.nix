783c9933e0f619af08396aa53523faa81704a4bb
Sherub Thakur <sherub.thakur@gmail.com>
2021-03-12 00:55:14 +0530

    Setup nu-shell v0.25.1

--- home.nix
+++ home.nix
@@ -355,8 +355,60 @@ in
     enable = true;
     settings = {
       edit_mode = "vi";
-      prompt = "echo $(starship prompt)";
+      # figure out how to use z add here
+      prompt = "za $(pwd) | echo $(starship prompt)";
+      startup = [
+        "def z [a] {if $(echo $a | empty?) == $true { cd ~ } {echo $a | each { if $it == '-' { cd - } {cd $(zoxide query -- $it | str trim)}}}}"
+        "def zi [a] {echo $(do -i {zoxide query -i $a} | str trim) | each { cd $it }}"
+        "def za [a:block] {zoxide add $a}"
+        "def zq [a] {zoxide query -- $a}"
+        "def zl [] {zoxide query --list}"
+        "def zqi [a] {echo $(do -i {zoxide query -i $a | str trim }) | each { cd $it }}"
+        "def zr [a] {zoxide remove $a}"
+        "def zri [a] {zoxide remove -i $a}"
+        "def dce [container command] { docker-compose exec $container /bin/bash -c '$command' }"
+        "def dcpytest [container] { docker-compose exec $container /bin/bash -c 'pytest' }"
+        "def dcpytestlf [container] { docker-compose exec $container /bin/bash -c 'pytest --lf' }"
+        "def dcpytestni [container] { docker-compose exec $container /bin/bash -c 'pytest -m \"not integration\"' }"
+      ] ++ (lib.mapAttrsToList (k: v: "alias ${k} = ${v}") (import ./zsh/aliases.nix));
+      color_config = {
+        # green|g, red|r, blue|u, black|b, yellow|y, purple|p, cyan|c, white|w
+        primitive_int = "wb";
+        primitive_decimal = "wb";
+        primitive_boolean = "cyan";
+        primitive_binary = "cyan";
+        primitive_date = "wd";
+        primitive_filesize = "rb";
+        primitive_string = "gb";
+        primitive_line = "yellow";
+        primitive_columnpath = "cyan";
+        primitive_pattern = "white";
+        primitive_duration = "blue";
+        primitive_range = "purple";
+        primitive_path = "ub";
+        separator_color = "grey";
+        header_align = "l"; # left|l, right|r, center|c
+        header_color = "blue";
+        header_bold = true;
+        index_color = "wd";
+        # leading_trailing_space_bg = "white";
+      };
     };
+    package = pkgs.nushell.overrideAttrs(o: rec {
+      name = "nushell-${version}";
+      version = "0.25.1";
+      src = pkgs.fetchFromGitHub {
+        repo = "nushell";
+        owner = "nushell";
+        rev = "${version}";
+        sha256 = "sha256-pLKZQ/UxJvvraRQ/coKws4IhCvjLNvk2hjw1NuIpVHM=";
+      };
+      cargoDeps = o.cargoDeps.overrideAttrs (lib.const {
+        name = "${name}-vendor.tar.gz";
+        inherit src;
+        outputHash = "sha256-5zC5qF8Qr8Q3C0iRit1jXV87DJKZ43C5nPokmit328U=";
+      });
+    });
   };
 
   programs.zsh = {
