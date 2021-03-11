46e619827777f090bbfe1db41def201b34a55ec3
Sherub Thakur <sherub.thakur@gmail.com>
2021-03-12 00:54:40 +0530

    Update neovim

--- home.nix
+++ home.nix
@@ -339,8 +339,8 @@ in
       src = pkgs.fetchFromGitHub {
         owner = "neovim";
         repo = "neovim";
-        rev = "a6bd52d877875deecb65d367bca8eda5d89fb8bc";
-        sha256 = "5FSvHvvvzUHJT0q4y4bSMY6MYH5N8HNBK+pRyFURTd8=";
+        rev = "b535575acdb037c35a9b688bc2d8adc2f3dece8d";
+        sha256 = "sha256-8AbjEaHG/6h35CunubmzImHvIx8DS1QVHOgTEtxIM4w=";
       };
 
       buildInputs = o.buildInputs ++ [pkgs.tree-sitter];
