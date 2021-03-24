abc3a2a31fd15ebad6cafd01dca686afc50d99e5
Sherub Thakur <sherub.thakur@gmail.com>
2021-03-24 19:35:17 +0530

    Remove specific version of picom

--- home.nix
+++ home.nix
@@ -497,14 +497,6 @@ in
     '';
     fade = true;
     fadeDelta = 5;
-    package = pkgs.picom.overrideAttrs(o: {
-      src = pkgs.fetchFromGitHub {
-        repo = "picom";
-        owner = "ibhagwan";
-        rev = "44b4970f70d6b23759a61a2b94d9bfb4351b41b1";
-        sha256 = "0iff4bwpc00xbjad0m000midslgx12aihs33mdvfckr75r114ylh";
-      };
-    });
   };
 
   xsession = {
