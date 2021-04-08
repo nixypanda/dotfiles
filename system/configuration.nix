bddb83a4f085802cdf909132abd326916742855a
Sherub Thakur <sherub.thakur@gmail.com>
2021-04-08 14:19:26 +0530

    Add system configuration to the dotfiles

---
+++ system/configuration.nix
@@ -0,0 +1,161 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  openrgb-rules = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/CalcProgrammer1/OpenRGB/master/60-openrgb.rules";
+  };
+  solaar-rules = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/pwr/Solaar/master/rules.d/42-logitech-unify-permissions.rules";
+  };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ];
+
+  # Get me proprietary packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      devices = ["nodev"];
+      efiSupport = true;
+      enable = true;
+    };
+    # systemd-boot.enable = true;
+  };
+
+  boot.kernelModules = [ "i2c-dev" "i2c-piix4" ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp5s0.useDHCP = true;
+  networking.interfaces.wlp4s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+     vim
+     home-manager
+     openrgb
+     i2c-tools
+     ddccontrol
+   ];
+   environment.pathsToLink = ["/libexec"];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.dconf.enable = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  hardware.opengl = {
+    driSupport = true;
+  }; 
+
+  # Enable the X11 windowing system.
+  services.blueman.enable = true;
+  services.xserver = {
+    enable = true;
+    xkbOptions = "caps:swapescape";
+
+    displayManager.sessionCommands = ''
+      xrandr --output HDMI-1 --mode 1920x1080
+    '';
+    desktopManager.session = [
+      {
+        name = "home-manager";
+        start = ''
+          ${pkgs.runtimeShell} $HOME/.hm-xsession &
+          waitPID=$!
+        '';
+      }
+    ];
+  };
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.lightdm.enable = true;
+  # services.xserver.desktopManager.xfce.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.sherub = {
+    isNormalUser = true;
+    # Enable ‘sudo’, 'audio' for the user.
+    extraGroups = [ "wheel" "audio" "docker" ];
+  };
+
+  # Switch Pro Controller udev rules
+  services.udev.extraRules = ''
+    ${builtins.readFile solaar-rules}
+    ${builtins.readFile ./openrgb.rules}
+  '';
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
+}
