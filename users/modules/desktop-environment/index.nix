edc7af0af455376166393a16ea6dc299922b2e18
Sherub Thakur <sherub.thakur@gmail.com>
2021-04-21 19:57:45 +0530

    Blur and rounded corners

--- users/modules/desktop-environment/index.nix
+++ users/modules/desktop-environment/index.nix
@@ -109,26 +109,34 @@ in
 
     services.picom = {
       enable = true;
-      # inactiveOpacity = "0.55";
-      # activeOpacity = "0.85";
+      inactiveOpacity = "0.90";
+      activeOpacity = "0.95";
       blur = true;
       experimentalBackends = true;
       opacityRule = [
         "100:class_g   *?= 'Google-chrome'"
       ];
       extraOptions = ''
-        # blur-method = "dual_kawase";
-        # blur-strength = 8;
-        # corner-radius = 8;
-        # round-borders = 1;
-        #
-        # rounded-corners-exclude = [
-        #   "class_g = 'Polybar'",
-        #   "class_g = 'Google-chrome'"
-        # ];
+        blur-method = "dual_kawase";
+        blur-strength = 8;
+        corner-radius = 8;
+        round-borders = 1;
+        
+        rounded-corners-exclude = [
+          "class_g = 'Polybar'",
+          "class_g = 'Google-chrome'"
+        ];
       '';
       fade = true;
       fadeDelta = 5;
+      package = pkgs.picom.overrideAttrs(o: {
+        src = pkgs.fetchFromGitHub {
+          repo = "picom";
+          owner = "ibhagwan";
+          rev = "44b4970f70d6b23759a61a2b94d9bfb4351b41b1";
+          sha256 = "0iff4bwpc00xbjad0m000midslgx12aihs33mdvfckr75r114ylh";
+        };
+      });
     };
 
     xsession = {
