63180494fb98b5b8988a2f23dec33111ebb69c30
Sherub Thakur <sherub.thakur@gmail.com>
2021-08-25 18:27:11 +0530

    Minor fixes to sysmenu

--- users/modules/desktop-environment/polybar/scripts/sysmenu.sh
+++ users/modules/desktop-environment/polybar/scripts/sysmenu.sh
@@ -3,17 +3,14 @@ if [ -z "$@" ]; then
     echo -en "Logout\0icon\x1fsystem-log-out\n"
     echo -en "Suspend\0icon\x1fsystem-suspend\n"
     echo -en "Reboot\0icon\x1fsystem-restart\n"
-    echo -en "Lock\0icon\x1fsystem-lock\n"
 else
     if [ "$1" = "Shutdown" ]; then
         systemctl -i poweroff
-    elif [ "$1" = "Exit" ]; then
-        killall xmonad
+    elif [ "$1" = "Logout" ]; then
+        killall xmonad-x86_64-linux
     elif [ "$1" = "Reboot" ]; then
         systemctl reboot
     elif [ "$1" = "Suspend" ]; then
-        systemctl suspend
-    elif [ "$1" = "Lock" ]; then
-        i3lock-fancy -p -t ""
+        systemctl -i suspend
     fi
 fi
