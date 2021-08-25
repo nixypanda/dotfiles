a724b357e54ca28d5d5bada1bb6727ed93a14d6b
Sherub Thakur <sherub.thakur@gmail.com>
2021-08-25 19:30:48 +0530

    Remove redundant settings from flakes

--- users/flake.nix
+++ users/flake.nix
@@ -24,16 +24,6 @@
                 nixpkgs.config = { allowUnfree = true; };
                 nixpkgs.overlays = overlays;
 
-                # This value determines the Home Manager release that your
-                # configuration is compatible with. This helps avoid breakage
-                # when a new Home Manager release introduces backwards
-                # incompatible changes.
-                #
-                # You can update Home Manager without changing this value. See
-                # the Home Manager release notes for a list of state version
-                # changes in each release.
-                home.stateVersion = "20.09";
-
                 # Let Home Manager install and manage itself.
                 programs.home-manager.enable = true;
 
@@ -81,16 +71,6 @@
                 '';
                 nixpkgs.config = { allowUnfree = true; };
                 nixpkgs.overlays = overlays;
-
-                # This value determines the Home Manager release that your
-                # configuration is compatible with. This helps avoid breakage
-                # when a new Home Manager release introduces backwards
-                # incompatible changes.
-                #
-                # You can update Home Manager without changing this value. See
-                # the Home Manager release notes for a list of state version
-                # changes in each release.
-                home.stateVersion = "20.09";
 
                 # Let Home Manager install and manage itself.
                 programs.home-manager.enable = true;
