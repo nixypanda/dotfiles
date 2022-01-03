b32bfa95e88a44a754634d53b0e0b54f58808cdd
Sherub Thakur <sherub.thakur@gmail.com>
2022-01-04 01:39:01 +0530

    Add super-tab functionality

--- users/modules/nvim/lsp.lua
+++ users/modules/nvim/lsp.lua
@@ -109,6 +109,16 @@ local lspkind = require('lspkind')
 
 lspkind.init({})
 
+local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode,
+                          true)
+end
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -124,14 +134,32 @@ cmp.setup({
         -- `cmp.config.disable` value.
         ['<C-y>'] = cmp.config.disable,
         ['<C-e>'] = cmp.mapping({i = cmp.mapping.abort(), c = cmp.mapping.close()}),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = false
-        }),
-        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif vim.fn["vsnip#expandable"]() == 1 then
+                feedkey("<Plug>(vsnip-expand)", "")
+            elseif vim.fn["vsnip#available"](1) == 1 then
+                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif check_backspace() then
+                fallback()
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
+            else
+                fallback()
+            end
+        end, {"i", "s"})
     },
     sources = cmp.config.sources({
-       {name = 'nvim_lsp'}, {name = 'vsnip'}, {name = 'nvim_lua'}, {name = 'calc'}
+        {name = 'nvim_lsp'}, {name = 'vsnip'}, {name = 'nvim_lua'}, {name = 'calc'}
     }, {{name = 'buffer'}, {name = 'path'}})
 })
 
