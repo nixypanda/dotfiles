d4b17d047f71ebdb82b789574e0f121df745c5fe
Sherub Thakur <sherub.thakur@gmail.com>
2022-01-07 18:25:11 +0530

    Weather cli app

---
+++ users/modules/desktop-environment/weather-cli/default.nix
@@ -0,0 +1,21 @@
+{ pkgs }:
+
+pkgs.stdenv.mkDerivation {
+  name = "custom-weather-cli";
+  buildInputs = with pkgs; [
+    pkg-config
+    (
+      python3.withPackages (
+        python3Packages: with python3Packages; [
+          requests
+        ]
+      )
+    )
+  ];
+  unpackPhase = ":";
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${./weather.py} $out/bin/custom-weather-cli
+    chmod +x $out/bin/custom-weather-cli
+  '';
+}
