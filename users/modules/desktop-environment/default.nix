95318509c602ca78c7b27612466bfeabaa80d852
Sherub Thakur <sherub.thakur@gmail.com>
2022-01-28 16:30:54 +0530

    Hack: to fix environment issues with eww

--- users/modules/desktop-environment/default.nix
+++ users/modules/desktop-environment/default.nix
@@ -207,6 +207,17 @@ in
         myNormalBorderColor = "${colorscheme.bg-primary-bright}"
       '';
     };
+
+    # So eww daemon starts in the parent shell, It executes the scripts
+    # provided in that shell environment only. That environment does not have
+    # access to the session variables that I declare in zsh.
+    # TODO: Figure out the exact sequence of evnets that happen here
+    # To provide the required session variables to eww daemon, importing the
+    # variables here.
+    profileExtra = ''
+      export DOTFILES=~/.dotfiles
+      ${builtins.readFile ../../../.secrets/env-vars.sh}
+    '';
   };
 }
 
