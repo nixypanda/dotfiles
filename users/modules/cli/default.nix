d98cb132605ab274614f91d38c20c1f49705304f
Sherub Thakur <sherub.thakur@gmail.com>
2022-03-05 19:03:07 +0530

    add layout poetry to top levle direnv config

--- users/modules/cli/default.nix
+++ users/modules/cli/default.nix
@@ -38,6 +38,24 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    stdlib = ''
+      layout_poetry() {
+        if [[ ! -f pyproject.toml ]]; then
+          log_error 'No pyproject.toml found. Use `poetry new` or `poetry init` to create one first.'
+          exit 2
+        fi
+
+        local VENV=$(poetry env list --full-path | cut -d' ' -f1)
+        if [[ -z $VENV || ! -d $VENV/bin ]]; then
+          log_error 'No poetry virtual environment found. Use `poetry install` to create one first.'
+          exit 2
+        fi
+
+        export VIRTUAL_ENV=$VENV
+        export POETRY_ACTIVE=1
+        PATH_add "$VENV/bin"
+      }
+    '';
   };
 
   programs.nushell = {
