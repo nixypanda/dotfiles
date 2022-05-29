9afdfcfec3ea81161abc681fa8c7dff82d2aa5c0
Sherub Thakur <sherub.thakur@gmail.com>
2022-05-30 01:46:44 +0530

    Update nu shell configuration
    Have only added zoxide and starship integration. Will look at other
    defaults. The home manager options on it are outdated (very severly
    limited as well).
    
    The major roadblock that deters the adoption of nu shell as the primary
    shell still remains direnv integration. I doubt that there are any plans
    to add that support.

--- users/modules/cli/default.nix
+++ users/modules/cli/default.nix
@@ -57,48 +57,9 @@
 
   programs.nushell = {
     enable = true;
-    settings = {
-      edit_mode = "vi";
-      # figure out how to use z add here
-      # prompt = "echo $(starship prompt)";
-      startup = [
-        "def z [a] {if $(echo $a | empty?) == $true { cd ~ } {echo $a | each { if $it == '-' { cd - } {cd $(zoxide query -- $it | str trim)}}}}"
-        "def zi [a] {echo $(do -i {zoxide query -i $a} | str trim) | each { cd $it }}"
-        "def za [a:block] {zoxide add $a}"
-        "def zq [a] {zoxide query -- $a}"
-        "def zl [] {zoxide query --list}"
-        "def zqi [a] {echo $(do -i {zoxide query -i $a | str trim }) | each { cd $it }}"
-        "def zr [a] {zoxide remove $a}"
-        "def zri [a] {zoxide remove -i $a}"
-        "def dce [container command] { docker-compose exec $container /bin/bash -c '$command' }"
-        "def dcpytest [container] { docker-compose exec $container /bin/bash -c 'pytest' }"
-        "def dcpytestlf [container] { docker-compose exec $container /bin/bash -c 'pytest --lf' }"
-        "def dcpytestni [container] { docker-compose exec $container /bin/bash -c 'pytest -m \"not integration\"' }"
-      ] ++ (lib.mapAttrsToList (k: v: "alias ${k} = ${v}") (import ./zsh/aliases.nix));
-      color_config = {
-        # green|g, red|r, blue|u, black|b, yellow|y, purple|p, cyan|c, white|w
-        primitive_int = "wb";
-        primitive_decimal = "wb";
-        primitive_boolean = "cyan";
-        primitive_binary = "cyan";
-        primitive_date = "wd";
-        primitive_filesize = "rb";
-        primitive_string = "gb";
-        primitive_line = "yellow";
-        primitive_columnpath = "cyan";
-        primitive_pattern = "white";
-        primitive_duration = "blue";
-        primitive_range = "purple";
-        primitive_path = "ub";
-        separator_color = "grey";
-        header_align = "l"; # left|l, right|r, center|c
-        header_color = "blue";
-        header_bold = true;
-        index_color = "wd";
-        # leading_trailing_space_bg = "white";
-      };
-    };
   };
+  home.file."Library/Application Support/nushell/env.nu".source = ./nu/env.nu;
+  home.file."Library/Application Support/nushell/config.nu".source = ./nu/config.nu;
 
   programs.zsh = {
     enable = true;
