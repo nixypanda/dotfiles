d969f463cc5bd27086e9335553b3847d2c76c7ad
Sherub Thakur <sherub.thakur@gmail.com>
2022-07-01 01:05:00 +0530

    Add border to lspconfig info screen

--- users/modules/nvim/ide-lsp.lua
+++ users/modules/nvim/ide-lsp.lua
@@ -208,6 +208,16 @@ for _, elm in ipairs(diagnostic_symbol_map) do
     vim.fn.sign_define(elm.name, { texthl = elm.name, text = elm.symbol, numhl = elm.name })
 end
 
+-- Add border to lspconfig info screen
+local lspconfig_window = require("lspconfig.ui.windows")
+local old_defaults = lspconfig_window.default_opts
+
+function lspconfig_window.default_opts(opts)
+    local win_opts = old_defaults(opts)
+    win_opts.border = "rounded"
+    return win_opts
+end
+
 require('lspkind').init({})
 
 -- Display LSP messages overlayd on the current buffer (instead of the status
