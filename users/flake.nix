8b068c983ee96d4d3c140aadbefe1f494d28335a
Sherub Thakur <sherub.thakur@gmail.com>
2022-07-25 22:51:53 +0530

    Expract home configurations out to functions

--- users/flake.nix
+++ users/flake.nix
@@ -36,6 +36,57 @@
         ./modules/system-management
         ./modules/work.nix
       ];
+
+      home-linux = { lib, pkgs, ... }:
+        {
+          # NOTE: Here we are injecting colorscheme so that it is passed down all the imports
+          _module.args = {
+            colorscheme = import ./colorschemes/tokyonight.nix;
+          };
+
+          nixpkgs.config.allowUnfreePredicate = unfreePredicate lib;
+          nixpkgs.overlays = overlays;
+
+          # Let Home Manager install and manage itself.
+          programs.home-manager.enable = true;
+
+          imports = common_modules ++ [
+            ./modules/browser.nix
+            ./modules/desktop-environment
+            ./modules/media.nix
+          ];
+
+          # Packages that don't fit in the modules that we have
+          home.packages = with pkgs; [
+            discord
+            p3x-onenote
+          ];
+        };
+
+      home-macbook = { lib, pkgs, ... }:
+        {
+          # NOTE: Here we are injecting colorscheme so that it is passed down all the imports
+          _module.args = {
+            colorscheme = import ./colorschemes/tokyonight.nix;
+          };
+          xdg.configFile."nix/nix.conf".text = ''
+            experimental-features = nix-command flakes ca-references
+          '';
+          nixpkgs.config.allowUnfreePredicate = unfreePredicate lib;
+          nixpkgs.overlays = overlays;
+
+          # Let Home Manager install and manage itself.
+          programs.home-manager.enable = true;
+
+          home.packages = with pkgs; [
+            docker
+          ];
+
+          imports = common_modules ++ [
+            ./modules/tmux.nix
+          ];
+        };
+
     in
     {
       homeConfigurations = {
@@ -43,60 +94,14 @@
           system = "x86_64-linux";
           homeDirectory = "/home/sherub";
           username = "sherub";
-          configuration = { config, lib, pkgs, ... }@configInput:
-            {
-              # NOTE: Here we are injecting colorscheme so that it is passed down all the imports
-              _module.args = {
-                colorscheme = import ./colorschemes/tokyonight.nix;
-              };
-
-              nixpkgs.config.allowUnfreePredicate = unfreePredicate lib;
-              nixpkgs.overlays = overlays;
-
-              # Let Home Manager install and manage itself.
-              programs.home-manager.enable = true;
-
-              imports = common_modules ++ [
-                ./modules/browser.nix
-                ./modules/desktop-environment
-                ./modules/media.nix
-              ];
-
-              # Packages that don't fit in the modules that we have
-              home.packages = with pkgs; [
-                discord
-                p3x-onenote
-              ];
-            };
+          configuration = home-linux;
         };
 
         macbook-pro = inputs.home-manager.lib.homeManagerConfiguration {
           system = "x86_64-darwin";
           homeDirectory = "/Users/sherubthakur";
           username = "sherubthakur";
-          configuration = { config, lib, pkgs, ... }:
-            {
-              # NOTE: Here we are injecting colorscheme so that it is passed down all the imports
-              _module.args = {
-                colorscheme = import ./colorschemes/tokyonight.nix;
-              };
-              xdg.configFile."nix/nix.conf".text = ''
-                experimental-features = nix-command flakes ca-references
-              '';
-              nixpkgs.config.allowUnfreePredicate = unfreePredicate lib;
-              nixpkgs.overlays = overlays;
-
-              # Let Home Manager install and manage itself.
-              programs.home-manager.enable = true;
-
-              home.packages = with pkgs; [
-                docker
-              ];
-
-              imports = common_modules ++ [
-                ./modules/tmux.nix
-              ];
-            };
+          configuration = home-macbook;
         };
       };
     };
