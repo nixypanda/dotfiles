223a2559b5d6f48173def91687f5247bd32d2366
Sherub Thakur <sherub.thakur@gmail.com>
2022-07-26 00:29:51 +0530

    Symlink MacOS applications so launchers can find them

--- users/flake.nix
+++ users/flake.nix
@@ -51,16 +51,40 @@
           ];
         };
 
-      home-macbook = { pkgs, ... }:
+      home-macbook = { config, pkgs, lib, ... }:
         {
-          xdg.configFile."nix/nix.conf".text = ''
-            experimental-features = nix-command flakes ca-references
-          '';
           home.homeDirectory = "/Users/sherubthakur";
           home.username = "sherubthakur";
           imports = [
             ./modules/tmux.nix
           ];
+          xdg.configFile."nix/nix.conf".text = ''
+            experimental-features = nix-command flakes ca-references
+          '';
+          # Symlink macos applications. This does not happen by default.
+          # https://github.com/nix-community/home-manager/issues/1341
+          home.activation = {
+            copyApplications =
+              let
+                apps = pkgs.buildEnv {
+                  name = "home-manager-applications";
+                  paths = config.home.packages;
+                  pathsToLink = "/Applications";
+                };
+              in
+              lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+                baseDir="$HOME/Applications/Home Manager Apps"
+                if [ -d "$baseDir" ]; then
+                  rm -rf "$baseDir"
+                fi
+                mkdir -p "$baseDir"
+                for appFile in ${apps}/Applications/*; do
+                  target="$baseDir/$(basename "$appFile")"
+                  $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+                  $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+                done
+              '';
+          };
         };
 
       home-linux = { pkgs, ... }:
