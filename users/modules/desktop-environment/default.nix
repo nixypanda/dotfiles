16ae23c6a01df45f5836b785816361fac6b59155
Sherub Thakur <sherub.thakur@gmail.com>
2022-07-30 16:38:46 +0530

    fix nixos setup: apply-* scripts

--- users/modules/desktop-environment/default.nix
+++ users/modules/desktop-environment/default.nix
@@ -166,37 +166,25 @@ in
 
   services.picom = {
     enable = true;
-    inactiveOpacity = "0.50";
-    activeOpacity = "0.90";
-    blur = true;
+    inactiveOpacity = 0.90;
+    activeOpacity = 0.95;
     experimentalBackends = true;
-    opacityRule = [
+    opacityRules = [
       "100:class_g   *?= 'Google-chrome'"
       "95:class_g   *?= 'Deadd-notification-center'"
       "75:class_g   *?= 'Rofi'"
     ];
-    extraOptions = ''
+    settings = {
       blur-method = "dual_kawase";
       blur-strength = 8;
       corner-radius = 8;
       round-borders = 1;
-
       rounded-corners-exclude = [
-        "class_g = 'Polybar'",
+        "class_g = 'Polybar'"
       ];
-    '';
+    };
     fade = true;
     fadeDelta = 5;
-    package = pkgs.picom.overrideAttrs (
-      o: {
-        src = pkgs.fetchFromGitHub {
-          repo = "picom";
-          owner = "ibhagwan";
-          rev = "44b4970f70d6b23759a61a2b94d9bfb4351b41b1";
-          sha256 = "0iff4bwpc00xbjad0m000midslgx12aihs33mdvfckr75r114ylh";
-        };
-      }
-    );
   };
 
   services.xidlehook = {
