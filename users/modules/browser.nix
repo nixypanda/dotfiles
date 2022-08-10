387c2fce7bb640f258dcf05fbf35ffd605ba0135
Sherub Thakur <sherub.thakur@gmail.com>
2022-08-11 02:14:49 +0530

    Updated firefox config options

--- users/modules/browser.nix
+++ users/modules/browser.nix
@@ -22,42 +22,7 @@
     profiles = {
       default = {
         name = "Default";
-        settings = {
-          # Privacy settings
-          "privacy.donottrackheader.enabled" = true;
-          "privacy.trackingprotection.enabled" = true;
-          "privacy.trackingprotection.socialtracking.enabled" = true;
-          "privacy.partition.network_state.ocsp_cache" = true;
-
-          # Disable all sorts of telemetry
-          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-          "browser.newtabpage.activity-stream.telemetry" = false;
-          "browser.ping-centre.telemetry" = false;
-          "toolkit.telemetry.archive.enabled" = false;
-          "toolkit.telemetry.bhrPing.enabled" = false;
-          "toolkit.telemetry.enabled" = false;
-          "toolkit.telemetry.firstShutdownPing.enabled" = false;
-          "toolkit.telemetry.hybridContent.enabled" = false;
-          "toolkit.telemetry.newProfilePing.enabled" = false;
-          "toolkit.telemetry.reportingpolicy.firstRun" = false;
-          "toolkit.telemetry.shutdownPingSender.enabled" = false;
-          "toolkit.telemetry.unified" = false;
-          "toolkit.telemetry.updatePing.enabled" = false;
-
-          # As well as Firefox 'experiments'
-          "experiments.activeExperiment" = false;
-          "experiments.enabled" = false;
-          "experiments.supported" = false;
-          "network.allow-experiments" = false;
-
-          # Disable Pocket Integration
-          "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-          "extensions.pocket.enabled" = false;
-          "extensions.pocket.api" = "";
-          "extensions.pocket.oAuthConsumerKey" = "";
-          "extensions.pocket.showHome" = false;
-          "extensions.pocket.site" = "";
-        };
+        settings = import ./firefox/config.nix;
       };
     };
   };
