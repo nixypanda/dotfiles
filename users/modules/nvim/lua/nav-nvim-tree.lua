e95dc2b966a5741da8bba3f7196b7c8ff8f14407
Sherub Thakur <sherub.thakur@gmail.com>
2022-08-23 20:49:59 +0530

    Migrate indent markers setup to new config

--- users/modules/nvim/lua/nav-nvim-tree.lua
+++ users/modules/nvim/lua/nav-nvim-tree.lua
@@ -1,6 +1,8 @@
 require 'nvim-tree'.setup {
     -- update the focused file on `BufEnter`, un-collapses the folders
     -- recursively until it finds the file
-    update_focused_file = { enable = true }
+    update_focused_file = { enable = true },
+    renderer = {
+        indent_markers = { enable = true }
+    }
 }
-vim.g.nvim_tree_indent_markers = 1
