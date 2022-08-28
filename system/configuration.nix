21a137fee824325cecfafdc7ee8221bf2d91f7f1
Sherub Thakur <sherub.thakur@gmail.com>
2022-08-29 01:36:59 +0530

    xrandr setup to displayManager setup from displayManager session

--- system/configuration.nix
+++ system/configuration.nix
@@ -90,8 +90,8 @@
     autoRepeatDelay = 200;
     autoRepeatInterval = 20;
 
-    displayManager.sessionCommands = ''
-      xrandr --output DisplayPort-0 --mode 3840x2160 --scale 0.70x0.70 --output DisplayPort-1 --mode 2560x1440 --rotate left --left-of DisplayPort-0
+    displayManager.setupCommands = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --mode 3840x2160 --scale 0.70x0.70 --output DisplayPort-1 --mode 2560x1440 --rotate left --left-of DisplayPort-0
     '';
 
     desktopManager.session = [
