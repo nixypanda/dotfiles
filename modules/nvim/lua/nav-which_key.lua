a6ec920e8792538164436f9c2eaaafef668f24e6
Sherub Thakur <sherub.thakur@gmail.com>
2022-08-30 22:36:48 +0530

    Add reset hunk to which key table

--- modules/nvim/lua/nav-which_key.lua
+++ modules/nvim/lua/nav-which_key.lua
@@ -82,6 +82,7 @@ local mappings = {
         j = { '<cmd>lua require"gitsigns".next_hunk()<CR>', "Next Hunk" },
         k = { '<cmd>lua require"gitsigns".prev_hunk()<CR>', "Prev Hunk" },
         p = { '<cmd>lua require"gitsigns".preview_hunk()<CR>', "Preview Hunk" },
+        r = { '<cmd>Gitsigns reset_hunk<CR>', "Reset Hunk" },
         s = { '<cmd>lua require"gitsigns".stage_hunk()<CR>', "Stage Hunk" },
         u = { '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>', "Undo Stage Hunk" },
         c = {
