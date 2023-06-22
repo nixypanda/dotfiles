5587e81b7421b7538754e22b24e9863d971bc6b6
Sherub Thakur <sherub.thakur@gmail.com>
2023-06-23 02:49:35 +0530

    Remove annoying drawing commands on every change

--- modules/nvim/lua/draw-venn.lua
+++ modules/nvim/lua/draw-venn.lua
@@ -1,16 +1,19 @@
 -- venn.nvim: enable or disable keymappings
+
+local keymappings_option = { noremap = true, silent = true }
+
 function _G.Toggle_venn()
     local venn_enabled = vim.inspect(vim.b.venn_enabled)
     if venn_enabled == "nil" then
         vim.b.venn_enabled = true
         vim.cmd [[setlocal ve=all]]
         -- draw a line on HJKL keystokes
-        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
-        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
-        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
-        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", keymappings_option)
+        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", keymappings_option)
+        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", keymappings_option)
+        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", keymappings_option)
         -- draw a box by pressing "f" with visual selection
-        vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
+        vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", keymappings_option)
     else
         vim.cmd [[setlocal ve=]]
         vim.cmd [[mapclear <buffer>]]
