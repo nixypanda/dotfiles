10e0734b670c172247fed2de4e1ed881e13fda32
Sherub Thakur <sherub.thakur@gmail.com>
2023-08-30 07:47:12 +0530

    Add completions to few cli programs

--- modules/nu/default.nix
+++ modules/nu/default.nix
@@ -1,9 +1,16 @@
-{
+{ pkgs, lib, ... }:
+let
+  completions = [ "btm" "git" "cargo" "man" "nix" "poetry" "zellij" ];
+  mk_completion_str = c:
+    "use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/${c}/${c}-completions.nu *";
+in {
   programs.nushell = {
     enable = true;
     extraConfig = ''
       ${builtins.readFile ./config.nu}
       ${builtins.readFile ./aliases.nu}
+      # completions
+      ${lib.concatStringsSep "\n" (builtins.map mk_completion_str completions)}
     '';
     extraEnv = ''
       ${builtins.readFile ./env.nu}
