6751af77b1ad2753038ae85095917a75fd9eff4d
Sherub Thakur <sherub.thakur@gmail.com>
2024-08-01 00:38:27 +0530

    Cleaner DBUI setup

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -303,8 +303,10 @@ in
         type = "lua";
         config = # lua
           ''
-            vim.keymap.set("n", "<leader>Da",  "<cmd>DBUIAddConnection<cr>",{ desc = "[A]dd new connection" })
-            vim.keymap.set("n", "<leader>Dt",  "<cmd>DBUIToggle<cr>",{ desc = "Toggle DBUI" })
+            vim.g.db_ui_use_nerd_fonts = 1
+            vim.g.db_ui_win_position = "right"
+            vim.keymap.set("n", "<leader>Da",  "<cmd>DBUIAddConnection<cr>",{ desc = "Add new connection" })
+            vim.keymap.set("n", "<leader>Do",  "<cmd>:tab DBUI<cr>",{ desc = "Open DBUI" })
           '';
       }
       vim-dadbod-completion
