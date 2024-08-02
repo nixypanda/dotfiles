3df7ff3c9019d8953d35a34a7d62f87102d09673
Sherub Thakur <sherub.thakur@gmail.com>
2024-08-02 19:57:57 +0530

    Add a plugin to zellij

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -1,3 +1,23 @@
+{ pkgs, ... }:
+let
+
+  zellij-forgot = pkgs.stdenv.mkDerivation rec {
+    pname = "zellij-forgot";
+    version = "0.4.0";
+
+    src = builtins.fetchurl {
+      url = "https://github.com/karimould/zellij-forgot/releases/download/${version}/zellij_forgot.wasm";
+      sha256 = "sha256:1hzdvyswi6gh4ngxnplay69w1n8wlk17yflfpwfhv6mdn0gcmlsr";
+    };
+    phases = [ "installPhase" ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/zellij_forgot.wasm
+    '';
+  };
+
+in
 {
   programs.zellij = {
     enable = true;
@@ -9,6 +29,20 @@
       pane_frames false
       theme "catppuccin-macchiato"
       default_layout "compact"
-      ${builtins.readFile ./keybinds.kdl}
+      simplified_ui true
+
+      keybinds clear-defaults=true {
+        ${builtins.readFile ./keybinds.kdl}
+
+        shared_except "locked" {
+            bind "Ctrl y" {
+                LaunchOrFocusPlugin "file:${zellij-forgot}/bin/zellij_forgot.wasm" {
+                    "LOAD_ZELLIJ_BINDINGS" "true"
+                    floating true
+                }
+            }
+        }
+      }
+
     '';
 }
