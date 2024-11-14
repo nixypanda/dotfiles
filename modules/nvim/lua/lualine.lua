4c35cbfbeadb670056e40b49e5976980f1ff4e6e
nixypanda <sherub.thakur@gmail.com>
2024-11-15 00:29:05 +0530

    nvim: lualine: Remove section separators

--- modules/nvim/lua/lualine.lua
+++ modules/nvim/lua/lualine.lua
@@ -4,11 +4,11 @@ require("lualine").setup({
 	options = {
 		disabled_filetypes = { "NvimTree" },
 		iconsEnabled = true,
+		section_separators = "",
 		component_separators = "",
-		section_separators = { left = "", right = "" },
 	},
 	sections = {
-		lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+		lualine_a = { "mode" },
 		lualine_b = { "filename", "branch" },
 		lualine_c = {
 			{
@@ -19,18 +19,6 @@ require("lualine").setup({
 		},
 		lualine_x = { "diff" },
 		lualine_y = { "filetype", "progress" },
-		lualine_z = {
-			{ "location", separator = { right = "" }, left_padding = 2 },
-		},
-	},
-	inactive_sections = {
-		lualine_a = { "filename" },
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
 		lualine_z = { "location" },
 	},
-	tabline = {},
-	extensions = {},
 })
