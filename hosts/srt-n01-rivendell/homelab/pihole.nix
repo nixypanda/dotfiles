01f41a6bfd3638cc780dd00c2280a1b0be54a561
nixypanda <sherub.thakur@gmail.com>
2026-08-26 01:35:02 +0530

    Add more blocklists

--- hosts/srt-n01-rivendell/homelab/pihole.nix
+++ hosts/srt-n01-rivendell/homelab/pihole.nix
@@ -8,6 +8,121 @@
 
 let
   pihole = config.services.pihole-ftl.piholePackage;
+  blocklists = [
+    {
+      url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
+      description = "StevenBlack unified hosts";
+    }
+
+    # Firebog's green/ticked lists: https://firebog.net/
+    # Suspicious
+    {
+      url = "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt";
+      description = "PolishFiltersTeam KADhosts";
+    }
+    {
+      url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts";
+      description = "Fademind's Spammers";
+    }
+    {
+      url = "https://v.firebog.net/hosts/static/w3kbl.txt";
+      description = "WaLLy3K's Blacklist";
+    }
+
+    # Advertising
+    {
+      url = "https://adaway.org/hosts.txt";
+      description = "AdAway";
+    }
+    {
+      url = "https://v.firebog.net/hosts/AdguardDNS.txt";
+      description = "Adguard DNS";
+    }
+    {
+      url = "https://v.firebog.net/hosts/Admiral.txt";
+      description = "LanikSJ's Admiral Anti-Adblock";
+    }
+    {
+      url = "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt";
+      description = "Anudeep ND's Blacklist";
+    }
+    {
+      url = "https://v.firebog.net/hosts/Easylist.txt";
+      description = "Easylist";
+    }
+    {
+      url = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext";
+      description = "Peter Lowe's Adservers";
+    }
+    {
+      url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts";
+      description = "Fademind's Unchecky Ads";
+    }
+    {
+      url = "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts";
+      description = "hostsVN";
+    }
+
+    # Tracking & Telemetry
+    {
+      url = "https://v.firebog.net/hosts/Easyprivacy.txt";
+      description = "Easyprivacy";
+    }
+    {
+      url = "https://v.firebog.net/hosts/Prigent-Ads.txt";
+      description = "Fabrice Prigent's Ads";
+    }
+    {
+      url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts";
+      description = "Fademind's 2o7 Network Trackers";
+    }
+    {
+      url = "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt";
+      description = "Crazy Max's Microsoft Telemetry";
+    }
+    {
+      url = "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt";
+      description = "Geoffrey Frogeye's First-Party Trackers";
+    }
+
+    # Malicious
+    {
+      url = "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt";
+      description = "DandelionSprout's Anti Malware Filter";
+    }
+    {
+      url = "https://v.firebog.net/hosts/Prigent-Crypto.txt";
+      description = "Fabrice Prigent's Cryptojacking";
+    }
+    {
+      url = "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts";
+      description = "Fademind's Risky Hosts";
+    }
+    {
+      url = "https://phishing.army/download/phishing_army_blocklist_extended.txt";
+      description = "Phishing Army's Extended Blocklist";
+    }
+    {
+      url = "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt";
+      description = "Quidsup Malicious";
+    }
+    {
+      url = "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt";
+      description = "Spam404";
+    }
+    {
+      url = "https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts";
+      description = "Echap's Stalkerware Indicators";
+    }
+    {
+      url = "https://urlhaus.abuse.ch/downloads/hostfile/";
+      description = "URLhaus Malicious URL blocklist";
+    }
+    {
+      url = "https://lists.cyberhost.uk/malware.txt";
+      description = "CyberHost.uk Malware Domains";
+    }
+  ];
   setupResolvConf = pkgs.writeText "pihole-ftl-setup-resolv.conf" ''
     nameserver 1.1.1.1
     nameserver 9.9.9.9
@@ -20,12 +135,7 @@ in
       openFirewallDNS = true;
       openFirewallWebserver = true;
       queryLogDeleter.enable = true;
-      lists = [
-        {
-          url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
-          description = "StevenBlack unified hosts";
-        }
-      ];
+      lists = blocklists;
       settings = {
         misc.readOnly = false;
         webserver.api.cli_pw = true;
@@ -95,26 +205,41 @@ in
 
           LoginAPI
 
-          payload='{"address":["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"],"comment":"StevenBlack unified hosts","groups":[0]}'
-          result=$(PostFTLData "lists?type=block" "$payload")
-          error="$($jq '.error' <<< "$result")"
-          if [[ "$error" != "null" ]]; then
-            error_key="$($jq -r '.error.key // ""' <<< "$result")"
-            error_message="$($jq -r '.error.message // ""' <<< "$result")"
-            error_hint="$($jq -r '.error.hint // ""' <<< "$result")"
-            duplicate_constraint="UNIQUE constraint failed: adlist.address, adlist.type"
-            already_present="The item is already present"
+          add_blocklist() {
+            description="$1"
+            payload="$2"
+            result=$(PostFTLData "lists?type=block" "$payload")
+            error="$($jq '.error' <<< "$result")"
+            if [[ "$error" != "null" ]]; then
+              error_key="$($jq -r '.error.key // ""' <<< "$result")"
+              error_message="$($jq -r '.error.message // ""' <<< "$result")"
+              error_hint="$($jq -r '.error.hint // ""' <<< "$result")"
+              duplicate_constraint="UNIQUE constraint failed: adlist.address, adlist.type"
+              already_present="The item is already present"
 
-            if [[ "$error_key" == "database_error" ]] \
-              && [[ "$error_hint" == *"$duplicate_constraint"* || "$error_hint" == "$already_present" ]]; then
-              echo "StevenBlack blocklist already exists"
+              if [[ "$error_key" == "database_error" ]] \
+                && [[ "$error_hint" == *"$duplicate_constraint"* || "$error_hint" == "$already_present" ]]; then
+                echo "$description blocklist already exists"
+              else
+                echo "Error adding $description blocklist: key=$error_key message=$error_message hint=$error_hint"
+                exit 1
+              fi
             else
-              echo "Error adding list: key=$error_key message=$error_message hint=$error_hint"
-              exit 1
+              echo "Added $description blocklist"
             fi
-          else
-            echo "Added StevenBlack blocklist"
-          fi
+          }
+
+          ${lib.concatMapStringsSep "\n" (list: ''
+            add_blocklist ${lib.escapeShellArg list.description} ${
+              lib.escapeShellArg (
+                builtins.toJSON {
+                  address = [ list.url ];
+                  comment = list.description;
+                  groups = [ 0 ];
+                }
+              )
+            }
+          '') blocklists}
 
           $pihole -g
         '';
